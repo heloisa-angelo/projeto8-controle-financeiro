bc7e55867b1c2a8ba0967161cb49a489dc99b79b
Ana Heloisa <heloisaangelo.17@gmail.com>
2024-09-16 16:43:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-let money = parseInt(prompt("Quanto de dinheiro ha disponivel?"))
-let option = parseInt(prompt(` Você tem ${money}R$\n opção 1:Adicionar\n opção 2:Remover\n opção 3:Encerrar`))
-while (option !== 3) {
-  switch (option) {
-    case 1:
-      let add = parseInt(prompt('Quanto você quer adicionar?'))
-      money += add
-      break
-    case 2:
-      let minus = parseInt(prompt('Quanto você quer remover?'))
-      money -= minus
-      break
-    default:
-      alert('Opção Invalida')   
-  }
-  option = parseInt(prompt(` Você tem ${money}R$\n opção 1:Adicionar\n opção 2:Remover\n opção 3:Encerrar`))
-}
+let moneyValue = parseFloat(prompt('Digite seu dinheiro inicial'))
+let option
+
+do {
+    option = prompt(`Saldo atual: R$${moneyValue.toFixed(2)}
+Digite:
+1. Caso queira adicionar dinheiro.
+2. Caso queira retirar dinheiro.
+3. Caso deseje sair.`)
+
+    switch(option) {
+        case '1':
+            let valueAdition = parseFloat(prompt('Digite o valor que deseja adicionar.'));
+            if (!isNaN(valueAdition) && valueAdition > 0) {
+                moneyValue += valueAdition;
+            } else {
+                alert('Por favor, insira um valor válido maior que zero.');
+            }
+            break
+        case '2':
+            let valueRemove = parseFloat(prompt('Digite o valor que deseja retirar.'));
+            if (!isNaN(valueRemove) && valueRemove > 0) {
+                if (valueRemove <= moneyValue) {
+                    moneyValue -= valueRemove
+                } else {
+                    alert('Saldo insuficiente para realizar a retirada.')
+                }
+            } else {
+                alert('Por favor, insira um valor válido maior que zero.')
+            }
+            break
+        case '3':
+            alert('Encerrando programa.')
+            break
+        default:
+            alert('Opção inválida. Por favor, escolha uma opção válida.')
+            break;
+    }
+} while (option !== '3')
